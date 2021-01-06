9bdb9e23395f8d3b2c4b891735abb0e56048ddb7
travis <travis@thurntech.com>
2021-01-06 09:43:58 -0600

    adjusted regex

--- src/getVersion.sh
+++ src/getVersion.sh
@@ -1,25 +1,20 @@
 #!/bin/sh
 FullTag=$(git describe --tags)
-MajorVersion=$(echo $FullTag | grep -oP "v\K([0-9]+)(?=\.([0-9]+)\.([0-9]+)-([0-9]+)-([a-zA-Z0-9]+))")
-MinorVersion=$(echo $FullTag | grep -oP "v([0-9]+)\.\K([0-9]+)(?=\.([0-9]+)-([0-9]+)-([a-zA-Z0-9]+))")
-PatchNumber=$(echo $FullTag | grep -oP "v([0-9]+)\.([0-9]+)\.\K([0-9]+)(?=-([0-9]+)-([a-zA-Z0-9]+))")
-BuildNumber=$(echo $FullTag | grep -oP "v([0-9]+)\.([0-9]+)\.([0-9]+)-\K([0-9]+)(?=-([a-zA-Z0-9]+))")
-UniqueIdentiyfer=$(echo $FullTag | grep -oP "v([0-9]+)\.([0-9]+)\.([0-9]+)-([0-9]+)-\K([a-zA-Z0-9]+)")
+MajorVersion=$(echo $FullTag | grep -oP "v\K([0-9]+)(?=\.([0-9]+)\.([0-9]+))")
+MinorVersion=$(echo $FullTag | grep -oP "v([0-9]+)\.\K([0-9]+)(?=\.([0-9]+))")
+PatchNumber=$(echo $FullTag | grep -oP "v([0-9]+)\.([0-9]+)\.\K([0-9]+)(?=-([0-9]+))")
 Version="${MajorVersion}.${MinorVersion}.${PatchNumber}"
-FullVersion="${Version}.${BuildNumber}"
+FullVersion="${Version}.0"
 echo "Major Version == ${MajorVersion}"
 echo "::set-output name=major-version::${MajorVersion}"
 echo "Minor Version == ${MinorVersion}"
 echo "::set-output name=minor-version::${MinorVersion}"
 echo "Patch Number == ${PatchNumber}"
 echo "::set-output name=patch-number::${PatchNumber}"
-echo "Build Number == ${BuildNumber}"
-echo "::set-output name=build-number::${BuildNumber}"
+echo "::set-output name=build-number::0"
 echo "Version == ${Version}"
 echo "::set-output name=version::${Version}"
 echo "Full Version == ${FullVersion}"
 echo "::set-output name=full-version::${FullVersion}"
 echo "Full Tag == ${FullTag}"
 echo "::set-output name=full-tag::${FullTag}"
-echo "Unique Identiyfer == ${UniqueIdentiyfer}"
-echo "::set-output name=unique-identifyer::${UniqueIdentiyfer}"
